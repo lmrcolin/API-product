2fc01e4ee6d8d61f03b25d751c190e15178bd376
maitreyi97 <maitreyi@ciencias.unam.mx>
2025-11-03 22:55:57 -0600

    identificador ProductImage

--- product/src/main/java/com/product/api/entity/ProductImage.java
+++ product/src/main/java/com/product/api/entity/ProductImage.java
@@ -2,6 +2,8 @@ package com.product.api.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,6 +13,8 @@ public class ProductImage {
     @Column(name = "product_id")
     private Integer product_id;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_image_id")
     private Integer product_image_id;
 
